cbb41991a14d6e38253f7c6d4d2e09515521edb9
Victor Cruz <vicdan215@gmail.com>
2021-02-05 00:26:49 -0600

    Init Strapi project

---
+++ cms/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '96115121a9b22a7b949f8a233a3de3bb'),
+    },
+  },
+});
---
+++ cms/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2cfe2570-53c7-45fb-b73a-63e90c14111f'
+};
