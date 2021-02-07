ecdd762b1a1a2e07503ed93a21f8e843b92111a2
Victor Cruz <vicdan215@gmail.com>
2021-02-06 21:02:43 -0600

    Init TailwindCss

--- home/tailwind.config.js
+++ home/tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['Oswald', 'sans-serif'],
+      serif: ['Cardo', 'serif'],
+    },
+    extend: {
+      colors: {
+        "blue-cookie": { DEFAULT: '#4478A6' },
+        "yellow-cookie": { DEFAULT: '#F2CD5C' },
+        "white-cookie": { DEFAULT: '#F2EEEB' },
+        "brown-cookie": { DEFAULT: '#D9967E' },
+        "red-cookie": { DEFAULT: '#F24444' },
+      },
+    },
   },
   variants: {
     extend: {},
