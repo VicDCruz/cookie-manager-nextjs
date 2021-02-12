1b43f43ec133770675289f847f95bdbbf3de6ec5
Victor Cruz <vicdan215@gmail.com>
2021-02-12 16:43:29 -0600

    New components

---
+++ blogs/components/card.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Card({ title, createdAt, description, content }) {
+  return (
+    <div className="shadow-md rounded-md bg-white p-3">
+      <div className="flex justify-between items-end">
+        <h1 className="font-bold text-3xl">{title}</h1>
+        <p className="text-blue-cookie">{createdAt}</p>
+      </div>
+      <h3 className="text-brown-cookie text-lg">{description}</h3>
+      <p className="text-sm font-extralight">{content}</p>
+    </div>
+  );
+}
+
+Card.propTypes = {
+  title: PropTypes.string,
+  createdAt: PropTypes.string,
+  description: PropTypes.string,
+  content: PropTypes.string,
+};
+
+export default Card;
---
+++ blogs/components/header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Image from 'next/image';
+
+function Header({ url = '', username = '' }) {
+  const avatar = url ? (
+    <Image className="w-3 h-3 rounded-full" src={url} width="50" height="50" />
+  ) : (
+    <div className="w-12 h-12 flex bg-brown-cookie rounded-full items-center justify-center">
+      <p className="font-medium text-2xl">{username.charAt(0).toUpperCase()}</p>
+    </div>
+  );
+  return (
+    <div className="px-2 py-1 flex justify-between items-center bg-yellow-cookie">
+      <div className="flex gap-1 items-center">
+        <Image src="/images/logo.png" width="50" height="50" />
+        <p className="font-medium text-xl">D-Blog</p>
+      </div>
+      <div className="flex gap-1.5">
+        <div className="h-6 w-6 self-center text-blue-cookie">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+          >
+            <path
+              fillRule="evenodd"
+              d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+        {avatar}
+      </div>
+    </div>
+  );
+}
+
+Header.propTypes = {
+  url: PropTypes.string,
+  username: PropTypes.string,
+};
+
+export default Header;
