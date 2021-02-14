e0fbf9a841b7b80b3a523f8714b7914c3e865412
Victor Cruz <vicdan215@gmail.com>
2021-02-13 22:54:27 -0600

    New rules

--- blogs/.eslintrc.js
+++ blogs/.eslintrc.js
@@ -34,6 +34,9 @@ module.exports = {
     'react/require-default-props': 0,
     'react/jsx-one-expression-per-line': 0,
     'arrow-parens': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
     'prettier/prettier': [
       'error',
       { singleQuote: true, parser: 'flow', arrowParens: 'avoid' },
