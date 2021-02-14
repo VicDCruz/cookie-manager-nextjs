fa3564e4043c06cceb59994414146ac04745c0eb
Victor Cruz <vicdan215@gmail.com>
2021-02-13 22:52:40 -0600

    Change font

--- blogs/components/card.js
+++ blogs/components/card.js
@@ -9,7 +9,7 @@ function Card({ title, createdAt, description, content }) {
         <p className="text-blue-cookie">{createdAt}</p>
       </div>
       <h3 className="text-brown-cookie text-lg">{description}</h3>
-      <p className="text-sm font-extralight">{content}</p>
+      <p className="text-sm font-light text-gray-400">{content}</p>
     </div>
   );
 }
